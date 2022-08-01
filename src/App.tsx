dd202bd35663f47291d526ce7647363b070aa20c
Kenny Lee <kenny_seven@icloud.com>
2022-08-01 14:10:56 +1000

    feat: added triangle and lines

--- src/App.tsx
+++ src/App.tsx
@@ -45,42 +45,43 @@ const Box = (props) => {
   );
 };
 
-function App() {
-  // const count = 10;
-
-  // const points = useMemo(() => {
-  //   const p = new Array(count).fill(0).map((v) => (0.5 - Math.random()) * 7.5);
-
-  //   console.log(p);
-
-  //   const res = new BufferAttribute(new Float32Array(p), 3);
-  //   console.log(res);
-  //   return res;
-  // }, [count]);
-
+const Triangle = () => {
   const vertices = new Float32Array([0, 0, 0, 0, 1, 1, 0, 1, -1]);
   const verticesAttribute = new BufferAttribute(vertices, 3);
 
+  return (
+    <mesh>
+      <bufferGeometry>
+        <bufferAttribute attach="attributes-position" {...verticesAttribute} />
+      </bufferGeometry>
+      <meshBasicMaterial color={"hotpink"} side={THREE.DoubleSide} />
+    </mesh>
+  );
+};
+
+const Lines = () => {
+  const vertices = new Float32Array([2, 3, 2, 4, 3, 3, 2, -2, 2]);
+  const verticesAttribute = new BufferAttribute(vertices, 3);
+
+  return (
+    <line>
+      <bufferGeometry>
+        <bufferAttribute attach="attributes-position" {...verticesAttribute} />
+      </bufferGeometry>
+      <lineBasicMaterial color={"hotpink"} />
+    </line>
+  );
+};
+
+function App() {
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
       <Canvas style={{ background: "black" }} camera={{ position: [3, 3, 3] }}>
         <Orbit />
         <axesHelper args={[3]} />
         <Box position={[-1, 1, 2]} />
-        <line>
-          <bufferGeometry>
-            {/* <bufferAttribute
-              attach="attributes"
-              array={new Float32Array([0, 1, 2])}
-            /> */}
-            <bufferAttribute
-              attach="attributes-position"
-              {...verticesAttribute}
-            />
-            <meshBasicMaterial attach="material" color="red" />
-          </bufferGeometry>
-          <lineBasicMaterial color={"hotpink"} />
-        </line>
+        <Triangle />
+        <Lines />
       </Canvas>
     </div>
   );
